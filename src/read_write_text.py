02814bfb0b0bf82d3036721e29e98294ea72b4d8
jianmink <jianmink@192.168.0.103>
2013-05-08 22:00:56 +0800

    refactor: add menu.

--- src/read_write_text.py
+++ src/read_write_text.py
@@ -4,13 +4,17 @@
 import os
 
 
-def write_text():
+def write_text_file():
     while True:
-        filename=raw_input("please input the filename> ")
+        filename=raw_input("please input the filename, (b) back to previous menu\n>")
         if os.path.exists(filename):
-            print "file already exist, please choose another one"
+            print "*** file already exist!"
+        elif filename== 'b':
+            return
         else:
-            break
+            yes_no = raw_input('add a new file '+ filename+ ' (y/n): ')
+            if yes_no == 'y':
+                break
     
     all=[]
     print "please input the text, '.' itself for exit"
@@ -23,78 +27,111 @@ def write_text():
             all.append(text)
         
     f=open(filename, 'w')
-    if f:
-        f.write('\n'.join(all)+'\n')
-        f.close()
+    f.write('\n'.join(all)+'\n')
+    f.close()
+    print "DONE!"
     
-def read_text():
+def read_text_file():
     filename = raw_input("please input the filename> ")
-    if os.path.exists(filename):
+    try:
         f=open(filename, 'r')
-    else:
-        print "file not exist!"
-        return
-    
-    for text in f:
-        print text,
-    f.close()
+    except IOError, e:
+        print "*** file open error:", e
+    else:    
+        for entry in f:
+            print entry,
+        f.close()
 
-def edit_text():
-    filename = raw_input("please input the fileanme> ")
-    if not os.path.exists(filename):
-        print "file not exist"
-        return 
+
+def edit_menu():
+    print "---- edit menu ----"
+    print "(u)update"
+    print "(d)delete"
+    print "(a)add"
+    print "(s)save"
+    print "(b)main menu"
+    print "-------------------"  
+    return raw_input('> ')
+
+def edit_text_file():
+    " line editor of text file "
+    while True:
+        filename = raw_input("please input the fileanme> ")
+        if os.path.exists(filename):
+            break;
+        else:
+            print "file not exist"
     
-    
-    f=open(filename, 'r')
-    texts=f.readlines()
-    f.close()
-    i = 0
+    try:
+        f=open(filename, 'r')
+    except IOError, e:
+        print "*** file open error", e
+    else:        
+        texts=f.readlines()
+        f.close()
+        
+    i = 0    
     for text in texts:
         print "%03d:" %(i),text, 
         i+=1
     
-    print "please input the line num, '.' itself to exit"
     while True:
-        input_item = raw_input("> ")
-        if input_item == '.':
+        choice = edit_menu()
+        if choice == 'u':
+            entry = raw_input("line num> ")        
+            
+            if not entry.isdigit():
+                continue
+
+            number=int(entry)
+            if number >= len(texts) or number < 0:
+                print "illegal line number"
+            else:
+                print texts[number],
+                texts[number]=raw_input("new text> ")
+        elif choice == 's':
+            f=open(filename, 'w')
+            for text in texts:
+                f.write(text)
+                f.write('\n')
+            f.close()
+        elif choice == 'b':
             break
+        #todo : add, delete
         
-        number=int(input_item)
-        
-        if number < len(texts) and number >= 0:
-            print texts[number],
-        else:
-            print "illegal line number"
-            continue
-        
-        print "please input the text"
-        texts[number]=raw_input("> ")
-        
-    f=open(filename, 'w')
-    for text in texts:
-        f.write(text)
-    f.write('\n')
-    f.close()
-        
+
+def main_menu():
+    choice=raw_input("--------------- main menu --------------\n"
+                     "(r)read \t"
+                     "(n)new  \t"
+                     "(e)edit \n"
+                     "(x)exit \t"
+                     "(l)list \t"
+                     "(c)clear\n"
+                     "----------------------------------------\n"
+                     "> ")
+    if choice == 'r':
+        read_text_file()
+    elif choice == 'n':
+        write_text_file()
+    elif choice == 'e':
+        edit_text_file()
+    elif choice == 'x':
+        return False
+    elif choice == 'l':
+        os.system('ls -lh | grep -v py')
+    elif choice == 'c':
+        os.system('clear')
+    else:
+        pass
+    
+    return True
+    
 
         
 if __name__ == "__main__":
-    while True:
-        choice=raw_input("(1)read\n"
-                         "(2)write\n"
-                         "(3)edit\n"
-                         "(0)exit\n"
-                         "> ")
-        if choice == '1':
-            read_text()
-        elif choice == '2':
-            write_text()
-        elif choice == '3':
-            edit_text()
-        else:
-            break
-    
+    while main_menu():
+        pass
         
             
         
