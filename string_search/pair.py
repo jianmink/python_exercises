5496f2dd59a5c30d4dbd681505e688134879a4a0
jianmink <jianmink.taihe@gmail.com>
2015-02-11 21:37:36 +0800

    add no unique elements

--- string_search/pair.py
+++ string_search/pair.py
@@ -1,14 +1,24 @@
 
 import unittest
 
+#precondition: list is in order 
+
 def find_pair(s,a):
     r=[]
     i=0 
     j=len(s)-1
     while i < j:
         if s[i]+s[j]==a:
-            r.append([i,j])
+            r.append((s[i],s[j]))
+            
+            k=1
+            while s[i+k]==s[i] and i+k<=j: k+=1
+            i+=k
+            
+            k=1
+            while s[j-k]==s[j] and j-k>=i: k+=1 
             j-=1
+            
         elif s[i]+s[j]<a:
             i+=1
         else:
@@ -18,8 +28,14 @@ def find_pair(s,a):
 
 
 class TestPair(unittest.TestCase):
-    def test_find_pair(self):
+    def test_unique(self):
         s=[1,2,3,4,5,6,7,8]
         a=7
         r=find_pair(s,a)
-        self.assertEqual(r,[[0,5],[1,4],[2,3]])
+        self.assertEqual(r,[(1,6),(2,5),(3,4)])
+
+    def test_not_unique(self):
+        s=[3,3,3,4,4,4,4]
+        a=7
+        r=find_pair(s,a)
+        self.assertEqual(r,[(3,4)])
